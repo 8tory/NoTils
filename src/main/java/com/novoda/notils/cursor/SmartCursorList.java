f2ecada9d3f4c41feee344c037f9db39512a74bd
Andrew Chen <yongjhih@gmail.com>
2013-12-10 18:39:37 +0800

    Add SmartCursorList.OnCloseListener

--- src/main/java/com/novoda/notils/cursor/SmartCursorList.java
+++ src/main/java/com/novoda/notils/cursor/SmartCursorList.java
@@ -5,15 +5,41 @@ import java.util.List;
 import android.database.Cursor;
 
 public class SmartCursorList<T> extends SimpleCursorList<T> {
+    public interface OnCloseListener {
+        public void onClose();
+        public void onCloseFinished();
+        public static OnCloseListener NULL = new OnCloseListener() {
+            @Override
+            public void onClose() {
+            }
+            @Override
+            public void onCloseFinished() {
+            }
+        };
+    }
+
+    private OnCloseListener mCloseListener = OnCloseListener.NULL;
 
     public SmartCursorList(Cursor cursor, CursorMarshaller<T> marshaller) {
+        this(cursor, marshaller, null);
+    }
+
+    public SmartCursorList(Cursor cursor, CursorMarshaller<T> marshaller, OnCloseListener listener) {
         super(cursor, marshaller);
+        setOnCloseListener(listener);
     }
 
     private void _close() {
         if (!isClosed()) {
+            mCloseListener.onClose();
             close();
+            mCloseListener.onCloseFinished();
         }
+    }
+
+    public void setOnCloseListener(OnCloseListener listener) {
+        if (listener == null) return;
+        mCloseListener = listener;
     }
 
     @Override
