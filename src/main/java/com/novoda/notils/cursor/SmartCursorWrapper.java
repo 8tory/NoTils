cad168dac4cea9f92b0e13f3cac0046fc7171ae5
Wendly.Li <wendly777@gmail.com>
2013-12-26 17:15:09 +0800

    Add setOnCloseListener

--- src/main/java/com/novoda/notils/cursor/SmartCursorWrapper.java
+++ src/main/java/com/novoda/notils/cursor/SmartCursorWrapper.java
@@ -4,11 +4,40 @@ import android.database.Cursor;
 import android.database.CursorWrapper;
 
 public class SmartCursorWrapper extends CursorWrapper {
-    SmartCursorWrapper(Cursor cursor) { super(cursor); }
+    private OnCloseListener mCloseListener = OnCloseListener.NULL;
+
+    public interface OnCloseListener {
+        public void onClose();
+        public void onCloseFinished();
+
+        public static final OnCloseListener NULL = new OnCloseListener() {
+            @Override
+            public void onClose() {
+            }
+
+            @Override
+            public void onCloseFinished() {
+            }
+        };
+    }
+
+    public SmartCursorWrapper(Cursor cursor) {
+        super(cursor);
+    }
 
     private void _close() {
         if (!isClosed()) {
+            mCloseListener.onClose();
             close();
+            mCloseListener.onCloseFinished();
+        }
+    }
+
+    public void setOnCloseListener(OnCloseListener listener) {
+        if (listener == null) {
+            mCloseListener = OnCloseListener.NULL;
+        } else {
+            mCloseListener = listener;
         }
     }
 
